999b1ff38d34efb6f8b5ff3579a1c1c5fb7932bd
Mohammad <ma@bornak.de>
2025-02-14 18:43:47 +0330

    Initial Commit

---
+++ SpotifyApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SpotifyApp
+//
+//  Created by Mohammad on 2/14/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SpotifyApp/SpotifyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotifyAppApp.swift
+//  SpotifyApp
+//
+//  Created by Mohammad on 2/14/25.
+//
+
+import SwiftUI
+
+@main
+struct SpotifyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
